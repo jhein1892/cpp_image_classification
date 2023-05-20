908e4f52442ccfa1f683fb6c0b470214e7374942
Jacob Hein <jacobhein1892@gmail.com>
2023-05-19 18:09:31 -0700

    updated to run openCV starter code

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,26 @@
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+
 #include <iostream>
-#include <opencv2/opencv.hpp>
 
 using namespace cv;
-using namespace std;
 
 int main(){
-    string path = "Your_image_dir";
-    Mat img = imread(path);
-    imshow("image", img);
-    waitKey(0);
+    std::string image_path = samples::findFile("starry_night.jpg");
+    Mat img = imread(image_path, IMREAD_COLOR);
+
+    if(img.empty()){
+        std::cout << "Could not read the image: " << image_path << std::endl;
+        return 1;
+    }
+
+    imshow("Display Window", img);
+    int k = waitKey(0);
+    if(k == 's'){
+        imwrite("starry_night.png", img);
+    }
+
     return 0;
+
 }
