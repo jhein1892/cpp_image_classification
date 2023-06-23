4d054af980abdb7c80d3a6b635c6fdb4ed8b7d80
Jacob Hein <jacobhein1892@gmail.com>
2023-06-23 15:11:32 -0700

    able to read frame cound and fps rate

--- src/main.cpp
+++ src/main.cpp
@@ -9,33 +9,33 @@ using namespace cv;
 using namespace std;
 
 int main(){
-    string image_path = samples::findFile("starry_night.jpg");
-    Mat img = imread(image_path, IMREAD_COLOR);
-    Mat img_greyscale = imread(image_path, IMREAD_GRAYSCALE);
-    Mat image_unchanged = imread(image_path, IMREAD_UNCHANGED);
+    // string image_path = samples::findFile("starry_night.jpg");
+    // Mat img = imread(image_path, IMREAD_COLOR);
+    // Mat img_greyscale = imread(image_path, IMREAD_GRAYSCALE);
+    // Mat image_unchanged = imread(image_path, IMREAD_UNCHANGED);
 
-    if(img.empty()){
-        cout << "Could not read the image: " << image_path << endl;
-        return 1;
-    }
-
-    namedWindow("Color Image", WINDOW_AUTOSIZE);
-    namedWindow("greyscale image", WINDOW_AUTOSIZE);
-    namedWindow("unchanged image", WINDOW_AUTOSIZE);
-
-    imshow("Color Image", img);
-    imshow("greyscale image", img_greyscale);
-    imshow("unchanged image", image_unchanged);
-
-    int k = waitKey(0);
-    imwrite("starry_night.png", img);
-    // if(k == 's'){
+    // if(img.empty()){
+    //     cout << "Could not read the image: " << image_path << endl;
+    //     return 1;
     // }
 
-    destroyAllWindows();
+    // namedWindow("Color Image", WINDOW_AUTOSIZE);
+    // namedWindow("greyscale image", WINDOW_AUTOSIZE);
+    // namedWindow("unchanged image", WINDOW_AUTOSIZE);
 
+    // imshow("Color Image", img);
+    // imshow("greyscale image", img_greyscale);
+    // imshow("unchanged image", image_unchanged);
 
-    VideoCapture vid_capture("Resources/Cars.mp4");
+    // int k = waitKey(0);
+    // imwrite("starry_night.png", img);
+    // // if(k == 's'){
+    // // }
+
+    // destroyAllWindows();
+
+    // string video_path = samples::findFile("Megamind.avi");
+    VideoCapture vid_capture("/Users/jacobhein/Downloads/IMG_0637.mov");
 
     if(!vid_capture.isOpened()){
         std::cout << "Error opening video stream" << endl;
@@ -47,6 +47,8 @@ int main(){
 
         std::cout << " Frame count: " << frame_count;
     }
+
+
 
 
 
