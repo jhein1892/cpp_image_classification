896556592a1dd917cc8b7d10eff5070fd7b5f149
Jacob Hein <jacobhein1892@gmail.com>
2023-06-22 19:05:33 -0700

    working through saving the file

--- src/main.cpp
+++ src/main.cpp
@@ -11,17 +11,26 @@ using namespace std;
 int main(){
     string image_path = samples::findFile("starry_night.jpg");
     Mat img = imread(image_path, IMREAD_COLOR);
+    Mat img_greyscale = imread(image_path, IMREAD_GRAYSCALE);
+    Mat image_unchanged = imread(image_path, IMREAD_UNCHANGED);
 
     if(img.empty()){
         cout << "Could not read the image: " << image_path << endl;
         return 1;
     }
 
-    imshow("Display Window", img);
+    namedWindow("Color Image", WINDOW_AUTOSIZE);
+    namedWindow("greyscale image", WINDOW_AUTOSIZE);
+    namedWindow("unchanged image", WINDOW_AUTOSIZE);
+
+    imshow("Color Image", img);
+    imshow("greyscale image", img_greyscale);
+    imshow("unchanged image", image_unchanged);
+
     int k = waitKey(0);
-    if(k == 's'){
-        imwrite("starry_night.png", img);
-    }
+    imwrite("starry_night.png", img);
+    // if(k == 's'){
+    // }
 
     destroyAllWindows();
 
