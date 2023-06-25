3ba7f660f8e372dd206e97c7b101d481a9d6dde1
Jacob Hein <jacobhein1892@gmail.com>
2023-06-25 09:56:47 -0700

    working on tileing images

--- src/main.cpp
+++ src/main.cpp
@@ -10,14 +10,19 @@ int main()
 
     std::cout << "Width: " << img.size().width << std::endl;
     std::cout << "Height: " << img.size().height << std::endl;
+    std::cout << "Channels: " << img.channels() << std::endl;
 
-    Mat cropped_img = img(Range(80,280), Range(150,330));
+    // Mat cropped_img = img(Range(80,280), Range(150,330));
 
-    imshow("Original Image", img);
-    imshow("Cropped Image", cropped_img);
+    // imshow("Original Image", img);
+    // imshow("Cropped Image", cropped_img);
 
     // Writes cropped Image
     // imwrite("../Cropped Image.jpg", cropped_img);
+
+    Mat img_copy = img.clone();
+    int imgheight = img.rows;
+    imt imgWidth = img.cols;
 
     waitKey(0);
     destroyAllWindows();
