cae6843155ce5c60872737bab6b542220627bdc4
Jacob Hein <jacobhein1892@gmail.com>
2023-06-25 09:48:36 -0700

    working on saving cropped image

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,14 @@ int main()
     std::cout << "Width: " << img.size().width << std::endl;
     std::cout << "Height: " << img.size().height << std::endl;
 
-    
+    Mat cropped_img = img(Range(80,280), Range(150,330));
+
+    imshow("Original Image", img);
+    imshow("Cropped Image", cropped_img);
+
+    imwrite("./Cropped Image.jpg", cropped_img);
+
+    waitKey(0);
 
     return 0;
 }
