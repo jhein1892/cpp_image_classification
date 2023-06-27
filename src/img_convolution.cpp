0085f06d1dcce9b48006fa48c4f89f5525881873
Jacob Hein <jacobhein1892@gmail.com>
2023-06-27 14:30:43 -0700

    moving into threasholding

---
+++ src/img_convolution.cpp
@@ -0,0 +1,82 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+
+int main(){
+
+    std::string image_path = samples::findFile("starry_night.jpg");
+    Mat image = imread(image_path);
+
+    if(image.empty()){
+        std::cout << "Empty Image" << std::endl;
+    }
+
+    // Apply identity filter using kernal
+    Mat kernal1 = (Mat_<double>(3,3) << 0,0,0,0,1,0,0,0,0);
+    Mat identity;
+    filter2D(image, identity, -1, kernal1, Point(-1, 1), 0, 4);
+    imshow("Original", image);
+    imshow("Identity", identity);
+    waitKey();
+    destroyAllWindows();
+
+    // Blurred using kernal
+    // Initialize matrix with all ones
+    Mat kernal2 = Mat::ones(5,5, CV_64F);
+    // Normalize the elements
+    kernal2 = kernal2 / 25;
+    Mat img;
+    filter2D(image, img, -1, kernal2, Point(-1, 1), 0, 4);
+
+    // Using built in operation
+    Mat img_blur;
+    blur(image, img_blur, Size(5,5));
+
+    imshow("Original", image);
+    imshow("Identity", identity);
+    imshow("Kernel blur", img);
+    imshow("CV Blur", img_blur);
+    waitKey();
+    destroyAllWindows();
+
+    // Apply Gaussian blur
+    Mat gaussian_blur;
+    GaussianBlur(image, gaussian_blur, Size(5,5),0.0, 0.0);
+    imshow("Original", image);
+    imshow("Kernel", img_blur);
+    imshow("Gaussian", gaussian_blur);
+    waitKey();
+    destroyAllWindows();
+
+    // Apply Median blut
+    Mat median_blur;
+    medianBlur(image, median_blur, 5);
+    imshow("Original", image);
+    imshow("Kernel", img_blur);
+    imshow("Gaussian", gaussian_blur);
+    imshow("Median", median_blur);
+    waitKey();
+    destroyAllWindows();
+    
+    // Apply sharpening using kernel
+    Mat sharp_img;
+    Mat kernel3 = (Mat_<double>(3,3) << 0, -1, 0, -1, 5, -1, 0, -1, 0);
+    filter2D(image, sharp_img, -1 , kernel3, Point(-1,-1), 0, BORDER_DEFAULT);
+    imshow("Original", image);
+    imshow("Sharpened", sharp_img);
+    waitKey();
+    destroyAllWindows();
+
+    // Apply bilateral filtering
+    Mat bilateral_filter;
+    bilateralFilter(image, bilateral_filter, 9, 75, 75);
+    imshow("Original", image);
+    imshow("Gaussian", gaussian_blur);
+    imshow("Bilateral Filter", bilateral_filter);
+    waitKey();
+    destroyAllWindows();
+
+
+    return 0;
+}
