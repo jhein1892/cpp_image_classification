ad5680c1952019188a471923fbaa3e3cb444d6ee
Jacob Hein <jacobhein1892@gmail.com>
2023-06-27 14:14:46 -0700

    using built in blur function

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@ int main(){
     }
 
     // Apply identity filter using kernal
-    Mat kernal1 = (Mat_<double>(3,3) <<0,0,0,0,1,0,0,0,0);
+    Mat kernal1 = (Mat_<double>(3,3) << 0,0,0,0,1,0,0,0,0);
     Mat identity;
     filter2D(image, identity, -1, kernal1, Point(-1, 1), 0, 4);
     imshow("Original", image);
@@ -26,13 +26,20 @@ int main(){
     Mat kernal2 = Mat::ones(5,5, CV_64F);
     // Normalize the elements
     kernal2 = kernal2 / 25;
-
     Mat img;
     filter2D(image, img, -1, kernal2, Point(-1, 1), 0, 4);
+
+    // Using built in operation
+    Mat img_blur;
+    blur(image, img_blur, Size(5,5));
+
     imshow("Original", image);
     imshow("Identity", identity);
     imshow("Kernel blur", img);
+    imshow("CV Blur", img_blur);
     waitKey();
+
+
     destroyAllWindows();
 
     return 0;
