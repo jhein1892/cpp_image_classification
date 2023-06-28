6bfa2a3107c50ea02fbe744a009825847ca7c734
Jacob Hein <jacobhein1892@gmail.com>
2023-06-27 19:37:13 -0700

    working with threshold content

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,37 @@ using namespace cv;
 
 int main( int argc, char** argv) 
 {
-    Mat src = imread("threashold.png", IMREAD_GRAYSCALE);
+    std::string image_path = samples::findFile("gradient.jpg");
+    Mat src = imread(image_path, IMREAD_GRAYSCALE);
     Mat dst;
+    
+    // Basic threashold example
+    threshold(src, dst, 0, 255, THRESH_BINARY);
+    imwrite("../opencv-threshold-example.jpg", dst);
 
-    imshow("Original", src);
+    // Thresholding with maxval set to 128
+    threshold(src, dst, 0, 128, THRESH_BINARY);
+    imwrite("../opencv-thresh-binary-maxval.jpg", dst);
+
+    // Thresholding with threshold value set 127
+    threshold(src, dst, 127, 255, THRESH_BINARY);
+    imwrite("../opencv-thresh-binary.jpg", dst);
+
+    // Thresholding using THRESH_BINARY_INV
+    threshold(src, dst, 127, 255, THRESH_BINARY_INV);
+    imwrite("../opencv-thresh-binary-inv.jpg", dst);
+
+    // Thresholding using THRESH_TRUNC
+    threshold(src, dst, 127, 255, THRESH_TRUNC);
+    imwrite("../opencv-thresh-trunc.jpg", dst);
+
+    // Thresholding using THRESH_TOZERO
+    threshold(src, dst, 127, 255, THRESH_TOZERO);
+    imwrite("../opencv-thresh-tozero.jpg", dst);
+    // Basic threashold example
+    threshold(src, dst, 127, 255, THRESH_TOZERO_INV);
+    imwrite("../opencv-thresh-to-zero-inv.jpg", dst);
+
     waitKey();
     return 0;
 }
