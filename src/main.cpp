0ee78d2ae7631987db045656f7f3cfe09b163391
Jacob Hein <jacobhein1892@gmail.com>
2023-06-28 13:23:16 -0700

    working on binary threshold

--- src/main.cpp
+++ src/main.cpp
@@ -7,34 +7,45 @@ int main( int argc, char** argv)
     std::string image_path = samples::findFile("gradient.jpg");
     Mat src = imread(image_path, IMREAD_GRAYSCALE);
     Mat dst;
-    
-    // Basic threashold example
-    threshold(src, dst, 0, 255, THRESH_BINARY);
-    imwrite("../opencv-threshold-example.jpg", dst);
 
-    // Thresholding with maxval set to 128
-    threshold(src, dst, 0, 128, THRESH_BINARY);
-    imwrite("../opencv-thresh-binary-maxval.jpg", dst);
-
-    // Thresholding with threshold value set 127
-    threshold(src, dst, 127, 255, THRESH_BINARY);
-    imwrite("../opencv-thresh-binary.jpg", dst);
-
-    // Thresholding using THRESH_BINARY_INV
-    threshold(src, dst, 127, 255, THRESH_BINARY_INV);
-    imwrite("../opencv-thresh-binary-inv.jpg", dst);
-
-    // Thresholding using THRESH_TRUNC
-    threshold(src, dst, 127, 255, THRESH_TRUNC);
-    imwrite("../opencv-thresh-trunc.jpg", dst);
-
-    // Thresholding using THRESH_TOZERO
-    threshold(src, dst, 127, 255, THRESH_TOZERO);
-    imwrite("../opencv-thresh-tozero.jpg", dst);
-    // Basic threashold example
-    threshold(src, dst, 127, 255, THRESH_TOZERO_INV);
-    imwrite("../opencv-thresh-to-zero-inv.jpg", dst);
-
+    // Binary Threshold
+    double thresh = 0;
+    double maxValue = 255;
+    threshold(src, dst, thresh, maxValue, THRESH_BINARY);
+    imshow("Original", src);
+    imshow("Binary", dst);
     waitKey();
+    
+    // // Basic threashold example
+    // threshold(src, dst, 0, 255, THRESH_BINARY);
+    // imwrite("../opencv-threshold-example.jpg", dst);
+
+    // // Thresholding with maxval set to 128
+    // threshold(src, dst, 0, 128, THRESH_BINARY);
+    // imwrite("../opencv-thresh-binary-maxval.jpg", dst);
+
+    // // Thresholding with threshold value set 127
+    // threshold(src, dst, 127, 255, THRESH_BINARY);
+    // imwrite("../opencv-thresh-binary.jpg", dst);
+
+    // // Thresholding using THRESH_BINARY_INV
+    // threshold(src, dst, 127, 255, THRESH_BINARY_INV);
+    // imwrite("../opencv-thresh-binary-inv.jpg", dst);
+
+    // // Thresholding using THRESH_TRUNC
+    // threshold(src, dst, 127, 255, THRESH_TRUNC);
+    // imwrite("../opencv-thresh-trunc.jpg", dst);
+
+    // // Thresholding using THRESH_TOZERO
+    // threshold(src, dst, 127, 255, THRESH_TOZERO);
+    // imwrite("../opencv-thresh-tozero.jpg", dst);
+    // // Basic threashold example
+    // threshold(src, dst, 127, 255, THRESH_TOZERO_INV);
+    // imwrite("../opencv-thresh-to-zero-inv.jpg", dst);
+    waitKey();
+
+
+
+
     return 0;
 }
