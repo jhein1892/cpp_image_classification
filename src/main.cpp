6bcc60d76c7a3e3a3f8fff497c086dccca7a244a
Jacob Hein <jacobhein1892@gmail.com>
2023-07-11 18:22:31 -0700

    starting on mouse pointer and scroll bar

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,11 @@
 
 using namespace cv;
 
+// Points to store the bounding box coordinates
+
+
+
 int main() {
-    
+    std::string image_path = samples::findFile("starry_night.jpg");
+    Mat image = imread(image_path);
 }
