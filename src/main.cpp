60bd6194ca5b5666000924bb577dd47ff307757e
Jacob Hein <jacobhein1892@gmail.com>
2023-07-12 17:23:13 -0700

    threshold image

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,13 @@ int main () {
     Mat img = imread(image_path);
     Mat img_grey;
     cvtColor(img, img_grey, COLOR_BGR2GRAY);
+
+    Mat thresh; 
+    threshold(img_grey, thresh, 150, 255, THRESH_BINARY);
+    imshow("Binary Image", thresh);
+    waitKey(0);
+    destroyAllWindows();
     
+
     return 0;
 }
