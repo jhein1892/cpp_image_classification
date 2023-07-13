8b7099c761c267af49500a77e30c23fbf895abca
Jacob Hein <jacobhein1892@gmail.com>
2023-07-13 16:29:59 -0700

    using chain_approx_simple contour

--- src/main.cpp
+++ src/main.cpp
@@ -66,7 +66,7 @@ int main () {
     // imwrite("red_channel.jpg", image_contour_red);
     // destroyAllWindows();
 
-    // Now let us try with CHAIN_APPROX_SIMPLE`
+    // Now let us try with CHAIN_APPROX_SIMPLE
     // detect the contours on the binary image using cv2.CHAIN_APPROX_NONE
     std::vector<std::vector<Point>> contours1;
     std::vector<Vec4i> hierarchy1;
@@ -79,6 +79,30 @@ int main () {
     imwrite("contours_simple_image1.jpg", image_copy1);
     destroyAllWindows();
 
+    // Using a proper image for visualizing CHAIN_APPROX_SIMPLE
+    std::string image1_path = samples::findFile("LinuxLogo.jpg");
+    Mat image1 = imread(image1_path);
+    Mat img_grey1;
+    cvtColor(image1, img_grey1, COLOR_BGR2GRAY);
+    Mat thresh1;
+    threshold(img_grey1, thresh1, 150, 255, THRESH_BINARY);
+    std::vector<std::vector<Point>> contours2;
+    std::vector<Vec4i> hierarchy2;
+    findContours(thresh1, contours2, hierarchy2, RETR_TREE, CHAIN_APPROX_NONE);
+    Mat image_copy2 = image1.clone();
+    drawContours(image_copy2, contours2, -1, Scalar(0, 255,0), 2);
+    imshow("None approximation", image_copy2);
+    waitKey(0);
+    destroyAllWindows();
+    Mat image_copy3 = image1.clone();
+    for(int i = 0; i < contours2.size(); i = i + 1){
+        for(int j = 0; j <contours2[i].size(); j = j + 1){
+            circle(image_copy3, (contours2[i][0], contours2[i][i]), 2, Scalar(0, 255, 0), 2);
+        }
+    }
+    imshow("CHAIN_APPROX_SIMPLE Point only", image_copy3);
+    waitKey(0);
+    destroyAllWindows();
 
     return 0;
 }
