b670d1aee7cfd471662901ade1a4b5124da84ea0
Jacob Hein <jacobhein1892@gmail.com>
2023-07-12 17:28:43 -0700

    working on getting the contours now

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,19 @@ int main () {
     imshow("Binary Image", thresh);
     waitKey(0);
     destroyAllWindows();
-    
+
+    // detect the contours on the binary image using cv2.CHAIN_APPROX_NONE
+    std::vector<std::vector<Point>> contours;
+    std::vector<Vec4i> hierarchy;
+    findContours(thresh, contours, hierarchy, RETR_TREE, CHAIN_APPROX_NONE);
+    // draw contours on the original image
+    Mat image_copy = img.clone();
+    drawContours(image_copy, contours, -1, Scalar(0, 255, 0), 2);
+    imshow("None approximation", image_copy);
+    waitKey(0);
+    imwrite("contours_none_image1.jpg", image_copy);
+    destroyAllWindows();
+
 
     return 0;
 }
