db34a005c2af3f01d5f263c2dab522ca31621190
Jacob Hein <jacobhein1892@gmail.com>
2023-07-12 17:21:11 -0700

    starting contour recognition

--- src/main.cpp
+++ src/main.cpp
@@ -4,5 +4,10 @@
 using namespace cv;
 
 int main () {
+    std::string image_path = samples::findFile("right.jpg");
+    Mat img = imread(image_path);
+    Mat img_grey;
+    cvtColor(img, img_grey, COLOR_BGR2GRAY);
+    
     return 0;
 }
