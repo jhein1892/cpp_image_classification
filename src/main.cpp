f1af9d2279693ac2d2c79ec8e844b829c83ae941
Jacob Hein <jacobhein1892@gmail.com>
2023-07-15 17:30:03 -0700

    should be able to see prediction

--- src/main.cpp
+++ src/main.cpp
@@ -25,5 +25,21 @@ int main()
     std::string image_path = samples::findFile("tiger.jpg");
     Mat image = imread(image_path);
     Mat blob = blobFromImage(image, 0.01, Size(224, 224), Scalar(104, 117, 123));
+
+    // set the input blob for the neural network
+    model.setInput(blob);
+    // forward pass the image blob through the model
+    Mat outputs = model.forward();
+    Point classIdPoint;
+    double final_prob;
+    minMaxLoc(outputs.reshape(1,1), 0, &final_prob, 0, &classIdPoint);
+    int label_id = classIdPoint.x;
+    // Print predicted class.
+    std::string out_text = format("%s, %.3f", (class_names[label_id].c_str()), final_prob);
+    // put the class name texton top of the image
+    putText(image, out_text, Point(25,50), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 255, 0), 2);
+    imshow("Image", image);
+    waitKey(0);
+    destroyAllWindows();
     return 0;
 }
