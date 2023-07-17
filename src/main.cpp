d8bd998505cbd4d8a4ea8f3b58e175b8ed9ec7a1
Jacob Hein <jacobhein1892@gmail.com>
2023-07-16 19:16:04 -0700

    looping through image to classify content

--- src/main.cpp
+++ src/main.cpp
@@ -30,6 +30,22 @@ int main(){
     Mat output = model.forward();
     Mat detectionMat(output.size[2], output.size[3], CV_32F, output.ptr<float>());
 
-    for
+    for(int i = 0; i < detectionMat.rows; i++){
+        int class_id = detectionMat.at<float>(i, 1);
+        float confidence = detectionMat.at<float>(i, 2);
+
+        if (confidence > 0.4){
+             int box_x = static_cast<int>(detectionMat.at<float>(i, 3) * image.cols);
+           int box_y = static_cast<int>(detectionMat.at<float>(i, 4) * image.rows);
+           int box_width = static_cast<int>(detectionMat.at<float>(i, 5) * image.cols - box_x);
+           int box_height = static_cast<int>(detectionMat.at<float>(i, 6) * image.rows - box_y);
+           rectangle(image, Point(box_x, box_y), Point(box_x+box_width, box_y+box_height), Scalar(255,255,255), 2);
+           putText(image, class_names[class_id-1].c_str(), Point(box_x, box_y-5), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0,255,255), 1);
+       }
+   }
+
+   imshow("image", image);
+   waitKey(0);
+   destroyAllWindows();
     return 0;
 }
