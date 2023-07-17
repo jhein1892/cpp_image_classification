8b3ddfe18ea61386df8eef2ae50c91b076572835
Jacob Hein <jacobhein1892@gmail.com>
2023-07-16 19:13:17 -0700

    still trying to figure out why model not loading

--- src/main.cpp
+++ src/main.cpp
@@ -16,18 +16,20 @@ int main(){
     {
        class_names.push_back(line);
     }
-
-    auto model = readNet("/Users/jacobhein/Desktop/OpenCV/opencv/samples/data/dnn/frozen_inference_graph.pb", "/Users/jacobhein/Desktop/OpenCV/opencv/samples/data/dnn/ssd_mobilenet_v2_coco_2018_03_29.pbtxt", "TensorFlow");
-
     std::string image_path = samples::findFile("object_detection.jpg");
     Mat image = imread(image_path);
     int image_height = image.cols;
     int image_width = image.rows;
 
     Mat blob = blobFromImage(image, 1.0, Size(300, 300), Scalar(127.5, 127.5, 127.5), true, false);
+
+    auto model = readNet("/Users/jacobhein/Desktop/OpenCV/opencv/samples/data/dnn/frozen_inference_graph.pb", "/Users/jacobhein/Desktop/OpenCV/opencv/samples/data/dnn/ssd_mobilenet_v2_coco_2018_03_29.pbtxt", "TensorFlow");
+
     model.setInput(blob);
 
     Mat output = model.forward();
     Mat detectionMat(output.size[2], output.size[3], CV_32F, output.ptr<float>());
+
+    for
     return 0;
 }
